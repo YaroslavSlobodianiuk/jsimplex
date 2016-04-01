1897f59220d56bf5a8c35d9ffdda3e3d4c0bea3b
Alexander Komarov <shower.mvfw@gmail.com>
2016-04-01 17:40:50 +0300

    Удаление лишнего конструктора

--- src/main/java/NullAnswer.java
+++ src/main/java/NullAnswer.java
@@ -1,9 +1,5 @@
 public class NullAnswer extends Answer {
 
-    public NullAnswer() {
-
-    }
-
     @Override
     public String toString(boolean integer, boolean csv) {
         return "Функция не ограничена";
