755a5b9bdf8297872ff4eaf8962b9bebfa2f60e2
Alexander Komarov <shower.mvfw@gmail.com>
2016-04-02 00:35:08 +0300

    Скелеты классов функции цены и ограничения

---
+++ src/main/java/CostFunction.java
@@ -0,0 +1,39 @@
+class CostFunction {
+	
+	private double[] coefs;
+	private boolean minimize;
+	
+	public CostFunction(double[] coefs, boolean minimize) {
+		this.coefs = coefs;
+		this.minimize = minimize;
+	}
+	
+	public String toString() {
+		String string = coefs[0] + "x" + 1 + " ";
+		
+		for (int i = 1; i < coefs.length; i++) {
+			if (coefs[i] != 0) {
+				if (Math.signum(coefs[i]) > 0)
+					string += "+";
+				string += coefs[i] + "x" + (i+1) + " ";
+			}
+		}
+		
+		string += "-->" + (minimize ? "min" : "max");
+		
+		return string;
+	}
+	
+	public double getCoef(int number) {
+		return coefs[number];
+	}
+	
+	public int getCoefCount() {
+		return coefs.length;
+	}
+	
+	public boolean shouldBeMinimized() {
+		return minimize;
+	}
+	
+}
---
+++ src/main/java/Limitation.java
@@ -0,0 +1,65 @@
+class Limitation {
+	
+	public enum LimitationSign { LE, EQ, ME }
+	
+	private double[] coefs;
+	private LimitationSign sign;
+	private double freeTerm;
+	
+	public Limitation(double[] coefs, LimitationSign sign, double freeTerm) {
+		this.coefs = coefs;
+		this.sign = sign;
+		this.freeTerm = freeTerm;
+	}
+	
+	public String toString() {
+		String string = coefs[0] + "x" + 1 + " ";
+		
+		for (int i = 0; i < coefs.length; i++) {
+			if (coefs[i] != 0) {
+				if (Math.signum(coefs[i]) > 0)
+					string += "+";
+				string += coefs[i] + "x" + (i+1) + " ";
+			}
+		}
+		
+		switch(sign) {
+			
+			case LE:
+			string += "<= ";
+			break;
+			
+			case EQ:
+			string += "= ";
+			break;
+			
+			case ME:
+			string += ">= ";
+			break;
+			
+			default:
+			break;
+		}
+		
+		string += freeTerm;
+		
+		return string;
+	}
+	
+	public double getCoef(int number) {
+		return coefs[number];
+	}
+	
+	public int getCoefCount() {
+		return coefs.length;
+	}
+	
+	public LimitationSign getSign() {
+		return sign;
+	}
+	
+	public double getFreeTerm() {
+		return freeTerm;
+	}
+	
+}
