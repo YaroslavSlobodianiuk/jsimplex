8630fb2ac7f0f58db6b5e3b8b86708a8f3d14254
Alexander Komarov <shower.mvfw@gmail.com>
2016-04-04 10:27:35 +0300

    Тесты исключений в генераторе тестов

--- bin/create_test.rb
+++ bin/create_test.rb
@@ -1,8 +1,10 @@
 #!/usr/bin/env ruby
-test_number = Dir['src/test/fixtures/default/test*.txt'].last.match(/\d+/)[0].to_i + 1
+is_exception_test = !ARGV.nil? && (ARGV[0] == '--exception' || ARGV[0] == '-e')
+
 input = ''
 output = ''
 
+puts 'Для окончания ввода используйте Ctrl+D.'
 puts 'Введите входные данные:'
 while line = gets
   input += line
@@ -13,7 +15,13 @@ while line = gets
   output += line
 end
 
-File.open("src/test/fixtures/default/test#{test_number.to_s}.txt", 'w') { |f| f.write input }
-File.open("src/test/outputs/default/test#{test_number.to_s}.txt", 'w') { |f| f.write output }
-File.open("src/test/outputs/csv/test#{test_number.to_s}.txt", 'w') { |f| f.write output.gsub(' = ', ', ') }
-File.open("src/test/outputs/integer/test#{test_number.to_s}.txt", 'w') { |f| f.write output.gsub(/\..*$/, '') }
+if is_exception_test
+  test_number = Dir['src/test/fixtures/exception/test*.txt'].last.match(/\d+/)[0].to_i + 1
+  File.open("src/test/fixtures/exception/test#{test_number.to_s}.txt", 'w') { |f| f.write input }
+else
+  test_number = Dir['src/test/fixtures/default/test*.txt'].last.match(/\d+/)[0].to_i + 1
+  File.open("src/test/fixtures/default/test#{test_number.to_s}.txt", 'w') { |f| f.write input }
+  File.open("src/test/outputs/default/test#{test_number.to_s}.txt", 'w') { |f| f.write output }
+  File.open("src/test/outputs/csv/test#{test_number.to_s}.txt", 'w') { |f| f.write output.gsub(' = ', ', ') }
+  File.open("src/test/outputs/integer/test#{test_number.to_s}.txt", 'w') { |f| f.write output.gsub(/\..*$/, '') }
+end
